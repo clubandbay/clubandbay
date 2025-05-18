cda8c3aeab9872cf2086b0793f45f361f81a5630
abdul8840 <abdul14941naseer@gmail.com>
2025-05-18 22:15:41 +0530

    make more ui components and change menu page

--- client/src/pages/MenuPage/MenuPage.jsx
+++ client/src/pages/MenuPage/MenuPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import MenuContent from '../../components/MenuContent/MenuContent'
+import MenuHero from '../../components/MenuContent/MenuHero'
+import HomeOffer from '../../components/HomeContent/HomeOffer'
+import MenuHighlights from '../../components/MenuContent/MenuHighlights'
 
 const MenuPage = () => {
   return (
     <div>
+      <MenuHero />
       <MenuContent />
+      <HomeOffer />
+      <MenuHighlights />
     </div>
   )
 }
